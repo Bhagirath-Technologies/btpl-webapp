51d8e2a7cd745cb8f8e5f10da59f007fa2b15dcd
MrVivekSuryavanshi <vivek.suryavanshi@bhagirathtechnologies.com>
2023-12-21 18:04:24 +0530

    minor changes with href and anchor to link

--- src/components/Hero_Section/ServiceCards.jsx
+++ src/components/Hero_Section/ServiceCards.jsx
@@ -8,6 +8,7 @@ import 'swiper/css/effect-cards';
 import './servicecard.css'
 // import required modules
 import { EffectCards } from 'swiper/modules';
+import { Link } from 'react-router-dom';
 
 export default function App() {
     return (
@@ -23,16 +24,16 @@ export default function App() {
                     <div class="col-12">
                         <div class="rbt-card variation-01 rbt-hover">
                             <div class="rbt-card-img">
-                                <a
-                                    href="https://www.talentspiral.com/courses/certification-course-in-hr"
+                                <Link
+                                    to="#"
                                 ><img
                                         loading="lazy"
                                         src="https://img.freepik.com/free-photo/person-front-computer-working-html_23-2150040428.jpg?w=1060&t=st=1703148317~exp=1703148917~hmac=78f60c8485d8072d88a2cb077cd347732717fbf6c3e829d946d812fb6eaa0fce"
-                                        alt="Card image"
+                                        alt="Service"
                                     />
                                     <div class="rbt-badge-3 bg-white">
                                         <span>-50%</span><span>Off</span>
-                                    </div></a>
+                                    </div></Link>
                             </div>
                             <div class="rbt-card-body">
                                 <div class="rbt-card-top">
@@ -45,19 +46,18 @@ export default function App() {
                                         <span class="rating-count">(15 Reviews)</span>
                                     </div>
                                     <div class="rbt-bookmark-btn">
-                                        <a
+                                        <Link
                                             class="rbt-round-btn"
                                             title="Bookmark"
-                                            href="https://www.talentspiral.com/courses/certification-course-in-hr"
+                                            to="#"
                                         ><i class="feather-bookmark"></i
-                                        ></a>
+                                        ></Link>
                                     </div>
                                 </div>
                                 <h4 class="rbt-card-title">
-                                    <a
-                                        href="https://www.talentspiral.com/courses/certification-course-in-hr"
-                                    >Revolutionize Your Website Design</a
-                                    >
+                                    <Link
+                                        to="#"
+                                    >Revolutionize Your Website Design</Link>
                                 </h4>
                                 <ul class="rbt-meta">
                                     <li><i class="feather-book"></i>Customized Development</li>
@@ -96,9 +96,9 @@ export default function App() {
                                 <div class="rbt-card-bottom mt-3">
                                     <div class="rbt-price">
                                         <button>
-                                            <a
+                                            <Link
                                                 class="rbt-btn btn-gradient hover-icon-reverse"
-                                                href="https://pages.razorpay.com/pl_M2cdt50GxwvTno/view"
+                                                to="#"
                                                 target="_blank"
                                                 style={{ height: '35px', lineHeight: '35px' }}
                                             ><span class="icon-reverse-wrapper"
@@ -107,14 +107,14 @@ export default function App() {
                                             ><i class="feather-arrow-right"></i></span
                                                     ><span class="btn-icon"
                                                     ><i class="feather-arrow-right"></i></span></span
-                                                ></a>
+                                                ></Link>
                                         </button>
                                     </div>
-                                    <a
+                                    <Link
                                         class="rbt-btn-link"
-                                        href="https://www.talentspiral.com/courses/certification-course-in-hr"
+                                        to="#"
                                     >View Details<i class="feather-arrow-right"></i
-                                    ></a>
+                                    ></Link>
                                 </div>
                             </div>
                         </div>
@@ -124,17 +124,16 @@ export default function App() {
                     <div class="col-12">
                         <div class="rbt-card variation-01 rbt-hover">
                             <div class="rbt-card-img">
-                                <a
-                                    href="https://www.talentspiral.com/courses/certification-course-in-hr"
+                                <Link
+                                    to="#"
                                 ><img
                                         loading="lazy"
                                         src="https://img.freepik.com/free-photo/person-front-computer-working-html_23-2150040428.jpg?w=1060&t=st=1703148317~exp=1703148917~hmac=78f60c8485d8072d88a2cb077cd347732717fbf6c3e829d946d812fb6eaa0fce"
-                                        alt="Card image"
+                                        alt="service"
                                     />
                                     <div class="rbt-badge-3 bg-white">
                                         <span>-50%</span><span>Off</span>
-                                    </div></a
-                                >
+                                    </div></Link >
                             </div>
                             <div class="rbt-card-body">
                                 <div class="rbt-card-top">
@@ -147,19 +146,18 @@ export default function App() {
                                         <span class="rating-count">(15 Reviews)</span>
                                     </div>
                                     <div class="rbt-bookmark-btn">
-                                        <a
+                                        <Link
                                             class="rbt-round-btn"
                                             title="Bookmark"
-                                            href="https://www.talentspiral.com/courses/certification-course-in-hr"
+                                            to="#"
                                         ><i class="feather-bookmark"></i
-                                        ></a>
+                                        ></Link>
                                     </div>
                                 </div>
                                 <h4 class="rbt-card-title">
-                                    <a
-                                        href="https://www.talentspiral.com/courses/certification-course-in-hr"
-                                    >Revolutionize Your Website Design</a
-                                    >
+                                    <Link
+                                        to="#"
+                                    >Revolutionize Your Website Design</Link>
                                 </h4>
                                 <ul class="rbt-meta">
                                     <li><i class="feather-book"></i>Customized Development</li>
@@ -198,23 +196,23 @@ export default function App() {
                                 <div class="rbt-card-bottom mt-3">
                                     <div class="rbt-price">
                                         <button>
-                                            <a
+                                            <Link
                                                 class="rbt-btn btn-gradient hover-icon-reverse"
-                                                href="https://pages.razorpay.com/pl_M2cdt50GxwvTno/view"
+                                                to="#"
                                                 target="_blank"
                                                 style={{ height: '35px', lineHeight: '35px' }}
                                             ><span class="icon-reverse-wrapper"
                                             ><span class="btn-text">Contact Now</span><span class="btn-icon"
                                             ><i class="feather-arrow-right"></i></span><span class="btn-icon"
                                             ><i class="feather-arrow-right"></i></span></span
-                                                ></a>
+                                                ></Link>
                                         </button>
                                     </div>
-                                    <a
+                                    <Link
                                         class="rbt-btn-link"
-                                        href="https://www.talentspiral.com/courses/certification-course-in-hr"
+                                        to="#"
                                     >View Details<i class="feather-arrow-right"></i
-                                    ></a>
+                                    ></Link>
                                 </div>
                             </div>
                         </div>
