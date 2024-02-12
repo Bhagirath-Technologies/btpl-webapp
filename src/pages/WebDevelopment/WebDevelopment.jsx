bd01944c362574ece0bc97373ed38474644c66db
MrVivekSuryavanshi <vivek.suryavanshi@bhagirathtechnologies.com>
2024-02-12 18:52:09 +0530

    updated images in webp format rental & web

--- src/pages/WebDevelopment/WebDevelopment.jsx
+++ src/pages/WebDevelopment/WebDevelopment.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import {Link} from 'react-router-dom';
 import CommonCard from '../../components/CommonCard';
 import webData from '../../data/WebServicedata';
 
@@ -23,26 +22,7 @@ const WebDevelopment = () => {
 				<div class="container-fluid px-lg-4">
 					<div class="row">
 						{/* <!-- Card item START --> */}
-                        
-						<CommonCard commonData={webService}/>
-						<div class="col-sm-6 col-md-4 col-lg-3 col-xxl-2">
-							<div class="card">
-								<div class="position-relative">
-									{/* <!-- Card Image --> */}
-									<img class="card-img-top rounded" src="https://img.freepik.com/free-photo/laptop-with-colorful-screen-white-background-3d-rendering_1142-50640.jpg?t=st=1707209836~exp=1707213436~hmac=674983b0e98495bc81e780e29b3f4c115110b6d3d8a4c59a4edfa7a810511b62&w=826" alt="rental assets" />
-										{/* <!-- Card Overlay --> */}
-										<div class="position-absolute top-50 start-50 translate-middle card-element-hover p-0">
-											<Link to="#" class="btn btn-white btn-sm btn-line">Grab Now</Link>
-										</div>
-								</div>
-								{/* <!-- Card body --> */}
-								<div class="card-body px-0 mt-n5 position-relative zindex-9">
-									<span class="display-9 mb-0 mt-2 font-alt fw-normal bg_blue rounded text-white px-2 py-1 ms-3 mb-3 d-inline-block">Mac Book</span>
-									<h5 class="card-title"><Link to="#">CORE i7 laptop (12th Gen)</Link></h5>
-									<p class="small">Speedily say has suitable disposal add boy. On forth doubt miles of child. Exercise joy man children rejoiced.</p>
-								</div>
-							</div>
-						</div>
+						<CommonCard commonData={webService} />
 						{/* <!-- Card item END --> */}
 					</div>
 				</div>
